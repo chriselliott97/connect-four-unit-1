fe6ef3b28d08d7dac1d2ab112221fdea413b6e77
chriselliott97 <chris.elliott6497@gmail.com>
2022-09-09 14:11:09 -0400

    add functions needed to play game

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEl = document.querySelectorAll(".board > div")
+const squareEls = document.querySelectorAll("section > div")
 const messageEl = document.querySelector("#message")
 const resetBtnEl = document.querySelector("button")
-console.log(resetBtnEl)
+const displayPlayerTurn = document.querySelector("#player-turn")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -20,3 +20,34 @@ console.log(resetBtnEl)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+function init() {
+  turn = 1
+  winner = null
+  board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, , null, null, null, null, null, null, null, null, null, null, 1]
+  render()
+}
+
+function render() {
+  board.forEach((sqr, idx) => {
+    if (squareEls[idx].classList.contains('used')) {
+      if (sqr === 1) {
+        squareEls[idx].classList.add('player-one')
+      }
+    }  
+  })
+}
+
+function handleClick() {
+
+}
+
+function getWinner() {
+
+}
+
+function resetGame() {
+
+}
