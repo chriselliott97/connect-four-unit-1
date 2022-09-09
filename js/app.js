fad10dcad3dc9268d40a46afdb2db85abc31156b
chriselliott97 <chris.elliott6497@gmail.com>
2022-09-09 13:39:28 -0400

    add cache element references

--- js/app.js
+++ js/app.js
@@ -4,14 +4,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-const board, turn, winner
+let board, turn, winner
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const squareEl = document.querySelectorAll(".board > div")
+const messageEl = document.querySelector("#message")
+const resetBtnEl = document.querySelector("button")
+console.log(resetBtnEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
